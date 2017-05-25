bcfe6da6f147c818fa63bbe06913451a60f46bd9
906 <906@SHACHAM25>
2017-05-25 16:43:31 +0300

    Add project files.

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+using Wcf;
+
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        ServiceHost Host;
+        public Class1()
+        {
+            Host = new ServiceHost(typeof(Calc));
+        }
+
+        public void Open()
+        {
+            if (Host.State != CommunicationState.Opened )
+            {
+                Host.Open();
+            }
+        }
+        public void Close()
+        {
+            if (Host.State == CommunicationState.Opened)
+            {
+                Host.Close();
+            }
+        }
+    }
+}
---
+++ Wcf/Calc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Wcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Calc" in both code and config file together.
+    public class Calc : Icalc
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+       
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
